95821b22558a52f6b611d84e37a70821176c454c
bando7507 <bando7507@gmail.com>
2023-03-23 00:18:13 +0100

    debut du backendsss

--- netflix-clone/pages/Auth.jsx
+++ netflix-clone/pages/Auth.jsx
@@ -122,6 +122,7 @@ function Auth() {
               >
                 {variant === 'login' ? 'Create an account' : 'Login'}
               </span>
+              ss
             </p>
           </div>
         </div>
